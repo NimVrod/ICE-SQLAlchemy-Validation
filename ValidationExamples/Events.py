b2d96a443481a46ba4e100fbab1a84c0ab40364e
golemq <dominikdzi3@gmail.com>
2026-04-09 22:31:47 +0200

    add example snippets library

---
+++ ValidationExamples/Events.py
@@ -0,0 +1,7 @@
+﻿from sqlalchemy import event
+from Models import User
+
+@event.listens_for(User, "before_insert")
+def validate_user(mapper, connection, target):
+    if target.age < 18 and target.balance > 10000:
+        raise ValueError("Suspicious account: too much money for minor")
